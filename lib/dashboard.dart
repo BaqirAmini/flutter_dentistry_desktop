106d85eea295149ad72de47b76616baf2962f54c
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-05-12 21:29:17 +0430

    Complete initial design of charts.

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
@@ -18,6 +16,25 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   PageController page = PageController();
+  List<_SalesData> data = [
+    _SalesData('Jan', 10),
+    _SalesData('Feb', 20),
+    _SalesData('Mar', 20),
+    _SalesData('Apr', 50),
+    _SalesData('May', 40),
+    _SalesData('Jun', 35)
+  ];
+
+  List<_PieData>  pieData = [
+    _PieData('Jan', 9000, 'خوراک'),
+    _PieData('Feb', 15000, 'آب'),
+    _PieData('Mar', 100000, 'مالیات'),
+    _PieData('Apr', 1200, 'و سایر')
+
+
+
+  ];
+
   SideMenuController sideMenu = SideMenuController();
 
   @override
@@ -281,79 +298,96 @@ class _DashboardState extends State<Dashboard> {
                  Container(
                    margin: const EdgeInsets.only(top: 10.0),
                    child:  Row(
-                     children: const [
+                     children: [
                        Card(
-                         margin: EdgeInsets.only(
+                         margin: const EdgeInsets.only(
                              left: 20.0),
                          child: SizedBox(
                            height: 350,
                            width: 550,
-                           /*child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.grey[10],
-                              child: const Icon(Icons.people_alt_outlined,
-                                  color: Colors.white),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(
-                                  left: 0.0,
-                                  top: 0.0,
-                                  right: 15.0,
-                                  bottom: 0.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text('مراجعین',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          color: Colors.white)),
-                                  Text('20',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          color: Colors.white)),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),*/
+                           child: Column(
+                             children: [
+                               SfCartesianChart(
+                                   primaryXAxis: CategoryAxis(),
+                                   // Chart title
+                                   title: ChartTitle(text: 'مراجعه بیماران در شش ماه گذشته'),
+                                   // Enable legend
+                                   legend: Legend(isVisible: true),
+                                   // Enable tooltip
+                                   tooltipBehavior: TooltipBehavior(enable: true),
+                                   series: <ChartSeries<_SalesData, String>>[
+                                     LineSeries<_SalesData, String>(
+                                         dataSource: data,
+                                         xValueMapper: (_SalesData sales, _) => sales.year,
+                                         yValueMapper: (_SalesData sales, _) => sales.sales,
+                                         name: 'بیماران',
+                                         // Enable data label
+                                         dataLabelSettings: const DataLabelSettings(isVisible: true))
+                                   ]),
+                               Expanded(
+                                 child: Padding(
+                                   padding: const EdgeInsets.all(8.0),
+                                   //Initialize the spark charts widget
+                                   child: SfSparkLineChart.custom(
+                                     //Enable the trackball
+                                     trackball: SparkChartTrackball(
+                                         activationMode: SparkChartActivationMode.tap),
+                                     //Enable marker
+                                     marker: SparkChartMarker(
+                                         displayMode: SparkChartMarkerDisplayMode.all),
+                                     //Enable data label
+                                     labelDisplayMode: SparkChartLabelDisplayMode.all,
+                                     xValueMapper: (int index) => data[index].year,
+                                     yValueMapper: (int index) => data[index].sales,
+                                     dataCount: 5,
+                                   ),
+                                 ),
+                               )
+                             ],
+                           ),
                          ),
                        ),
                        Card(
                          child: SizedBox(
                            height: 350,
                            width: 220,
-                           /*child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.grey[10],
-                              child: const Icon(Icons.people_alt_outlined,
-                                  color: Colors.white),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(
-                                  left: 0.0,
-                                  top: 0.0,
-                                  right: 15.0,
-                                  bottom: 0.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text('مراجعین',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          color: Colors.white)),
-                                  Text('20',
-                                      style: TextStyle(
-                                          fontSize: 16.0,
-                                          color: Colors.white)),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),*/
+                           child: Column(
+                             children: [
+                              SfCircularChart(
+                                   title: ChartTitle(text: 'مصارف داخلی کلینیک طی سه ماه'),
+                                   legend: Legend(isVisible: true),
+                                   series: <PieSeries<_PieData, String>>[
+                                     PieSeries<_PieData, String>(
+                                         explode: true,
+                                         explodeIndex: 0,
+                                         dataSource: pieData,
+                                         xValueMapper: (_PieData data, _) => data.xData,
+                                         yValueMapper: (_PieData data, _) => data.yData,
+                                         dataLabelMapper: (_PieData data, _) => data.text,
+                                         dataLabelSettings: DataLabelSettings(isVisible: true)),
+                                   ]
+                               )
+                               /*Expanded(
+                                 child: Padding(
+                                   padding: const EdgeInsets.all(8.0),
+                                   //Initialize the spark charts widget
+                                   child: SfSparkLineChart.custom(
+                                     //Enable the trackball
+                                     trackball: SparkChartTrackball(
+                                         activationMode: SparkChartActivationMode.tap),
+                                     //Enable marker
+                                     marker: SparkChartMarker(
+                                         displayMode: SparkChartMarkerDisplayMode.all),
+                                     //Enable data label
+                                     labelDisplayMode: SparkChartLabelDisplayMode.all,
+                                     xValueMapper: (int index) => data[index].year,
+                                     yValueMapper: (int index) => data[index].sales,
+                                     dataCount: 5,
+                                   ),
+                                 ),
+                               )*/
+                             ],
+                           ),
                          ),
                        ),
                      ],
@@ -367,4 +401,18 @@ class _DashboardState extends State<Dashboard> {
       ),
     );
   }
+}
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
+}
+
+class _PieData {
+  _PieData(this.xData, this.yData, [this.text]);
+  final String xData;
+  final num yData;
+  final String? text;
 }
