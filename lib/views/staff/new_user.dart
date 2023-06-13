52b38ebced516e2adb5949ad1f3cf5b07520f5e6
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-06-13 06:00:37 +0430

    Wrap it with card to enhance the UI.

--- lib/views/staff/new_user.dart
+++ lib/views/staff/new_user.dart
@@ -25,121 +25,132 @@ class _NewUserState extends State<NewUser> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Center(
-        child: Form(
-          key: _formKey,
-          child: SizedBox(
-            width: 500.0,
-            child: Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 20.0),
-                  child: const Text(
-                    'لطفا یوزرنیم، رمز و وظیفه را با دقت در خانه های مربوط شان درج نمایید.',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(20.0),
-                  child: TextFormField(
-                    controller: _userNameController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'یوزرنیم (نام کاربری)',
-                      suffixIcon: Icon(Icons.person_outline_sharp),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.grey)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.blue)),
+      child: Card(
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: SizedBox(
+              width: 500.0,
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 20.0),
+                    child: const Text(
+                      'لطفا یوزرنیم، رمز و وظیفه را با دقت در خانه های مربوط شان درج نمایید.',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(20.0),
-                  child: TextFormField(
-                    controller: _userNameController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'رمز',
-                      suffixIcon: Icon(Icons.password_rounded),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.grey)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.blue)),
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      controller: _userNameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'یوزرنیم (نام کاربری)',
+                        suffixIcon: Icon(Icons.person_outline_sharp),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.grey)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(20.0),
-                  child: TextFormField(
-                    controller: _unConfirmController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'تکرار رمز',
-                      hintText: 'هر آنچه که در اینجا وارد میکنید باید با رمز تان مطابقت کند',
-                      suffixIcon: Icon(Icons.password_rounded),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.grey)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.blue)),
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      controller: _userNameController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'رمز',
+                        suffixIcon: Icon(Icons.password_rounded),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.grey)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(20.0),
-                  child: InputDecorator(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'عنوان وظیفه',
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.grey)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          borderSide: BorderSide(color: Colors.blue)),
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      controller: _unConfirmController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'تکرار رمز',
+                        hintText:
+                            'هر آنچه که در اینجا وارد میکنید باید با رمز تان مطابقت کند',
+                        suffixIcon: Icon(Icons.password_rounded),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.grey)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                      ),
                     ),
-                    child: DropdownButtonHideUnderline(
-                      child: SizedBox(
-                        height: 26.0,
-                        child: DropdownButton(
-                          isExpanded: true,
-                          icon: const Icon(Icons.arrow_drop_down),
-                          value: positionDropDown,
-                          items: positionItems.map((String positionItems) {
-                            return DropdownMenuItem(
-                              value: positionItems,
-                              alignment: Alignment.centerRight,
-                              child: Text(positionItems),
-                            );
-                          }).toList(),
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              positionDropDown = newValue!;
-                            });
-                          },
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: InputDecorator(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'عنوان وظیفه',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.grey)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: SizedBox(
+                          height: 26.0,
+                          child: DropdownButton(
+                            isExpanded: true,
+                            icon: const Icon(Icons.arrow_drop_down),
+                            value: positionDropDown,
+                            items: positionItems.map((String positionItems) {
+                              return DropdownMenuItem(
+                                value: positionItems,
+                                alignment: Alignment.centerRight,
+                                child: Text(positionItems),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                positionDropDown = newValue!;
+                              });
+                            },
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  width: 400.0,
-                  height: 35.0,
-                  margin: const EdgeInsets.only(bottom: 20.0),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('ثبت کردن'),
-                  ),
-                )
-              ],
+                  Container(
+                    width: 400.0,
+                    height: 35.0,
+                    margin: const EdgeInsets.only(bottom: 20.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('ثبت کردن'),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
