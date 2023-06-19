594c6255c7df951bdf237fa869a2994ae7129fd8
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-06-19 20:18:25 +0430

    Design taxes page.

---
+++ lib/views/taxes/tax_details.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+class TaxDetails extends StatelessWidget {
+  const TaxDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 500.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: const Color.fromARGB(255, 240, 239, 239),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'نمبر تشخیصیه مالیه دهنده (TIN)',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Color.fromARGB(255, 118, 116, 116),
+                  ),
+                ),
+                Text('8008319497'),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 240.0,
+                padding: const EdgeInsets.all(10.0),
+                color: const Color.fromARGB(255, 240, 239, 239),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    Text(
+                      'مالیات سال',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Color.fromARGB(255, 118, 116, 116),
+                      ),
+                    ),
+                    Text('1402'),
+                  ],
+                ),
+              ),
+              Container(
+                width: 240.0,
+                padding: const EdgeInsets.all(10.0),
+                color: const Color.fromARGB(255, 240, 239, 239),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    Text(
+                      'فیصدی مالیات (%)',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Color.fromARGB(255, 118, 116, 116),
+                      ),
+                    ),
+                    Text('30%'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 240.0,
+                padding: const EdgeInsets.all(10.0),
+                color: const Color.fromARGB(255, 240, 239, 239),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    Text(
+                      'تاریخ تحویلی',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Color.fromARGB(255, 118, 116, 116),
+                      ),
+                    ),
+                    Text('2022/06/06'),
+                  ],
+                ),
+              ),
+              Container(
+                width: 240.0,
+                padding: const EdgeInsets.all(10.0),
+                color: const Color.fromARGB(255, 240, 239, 239),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    Text(
+                      'مجموع مالیات',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Color.fromARGB(255, 118, 116, 116),
+                      ),
+                    ),
+                    Text('100,000 افغانی'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: const Color.fromARGB(255, 240, 239, 239),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: const [
+                Text(
+                  'مبلع باقی',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Color.fromARGB(255, 118, 116, 116),
+                  ),
+                ),
+                Text('50,000 افغانی'),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: const Color.fromARGB(255, 240, 239, 239),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'تحویل مالیات توسط',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Color.fromARGB(255, 118, 116, 116),
+                  ),
+                ),
+                Text('علی احمدی'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/taxes/taxes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '/models/tax_data_model.dart';
+
+void main() => runApp(const TaxList());
+
+class TaxList extends StatelessWidget {
+  const TaxList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Tooltip(
+              message: 'رفتن به داشبورد',
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.home_outlined),
+              ),
+            ),
+            title: const Text('مالیات کلینیک'),
+          ),
+          body: const TaxDataTable(),
+        ),
+      ),
+    );
+  }
+}
