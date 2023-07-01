d4bad49833928ecd8ad7c9a0045b0836fec7b7b9
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-07-01 20:10:27 +0430

    Connect flutter with mysql.

--- lib/models/db_conn.dart
+++ lib/models/db_conn.dart
@@ -1,8 +1,6 @@
 import 'package:galileo_mysql/galileo_mysql.dart';
 
-void main() => onConnToDb();
-
-Future onConnToDb() async {
+Future<MySqlConnection> onConnToDb() async {
   try {
     final conn = await MySqlConnection.connect(ConnectionSettings(
         host: 'localhost',
@@ -11,16 +9,8 @@ Future onConnToDb() async {
         password: 'root',
         db: 'dentistry_db'));
     return conn;
-   /*   var query = await conn.query(
-        'INSERT INTO staff (username, password, role) VALUES (?, ?, ?)',
-        ['baqir123', '102330', 'Dentist']);
-    if (query.affectedRows! > 0) {
-      print('Insert successful!');
-    } else {
-      print('Insert failed.');
-    } 
-    await conn.close(); */
   } catch (e) {
     print(e);
+    rethrow;
   }
 }
