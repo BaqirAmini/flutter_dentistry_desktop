4d8b7a113810558e327b50930c4968441f0f88cf
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-08-03 04:54:05 +0430

    Create a static class for assigning taxes values.

---
+++ lib/views/finance/taxes/tax_info.dart
@@ -0,0 +1,15 @@
+class TaxInfo {
+  static int? taxPayID;
+  static int? taxID;
+  static int? staffID;
+  static String? TIN;
+  static double? annualIncomes;
+  static double? taxRate;
+  static String? paidDate;
+  static double? annTotTaxes;
+  static double? dueTaxes;
+  static String? taxOfYear;
+  static String? firstName;
+  static String? lastName;
+  static Function? onAddTax;
+}
