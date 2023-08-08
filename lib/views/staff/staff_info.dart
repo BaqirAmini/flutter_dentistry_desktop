e4dd2f2a0c2c3a66a1e52df22f3b47c45c803b82
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-08-08 05:16:19 +0430

    Create more static class members for staff.

--- lib/views/staff/staff_info.dart
+++ lib/views/staff/staff_info.dart
@@ -1,4 +1,11 @@
 class StaffInfo {
   static int? staffID;
   static String? staffRole;
+  static String? firstName;
+  static String? lastName;
+  static double? salary;
+  static String? position;
+  static String? phone;
+  static String? tazkira;
+  static String? address;
 }
