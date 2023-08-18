f1285a61b22f0c9907ea0481f07faa88b82bdc19
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-08-18 22:16:41 +0430

    Create a class with static members to set/get patients info.

---
+++ lib/views/patients/patient_info.dart
@@ -0,0 +1,12 @@
+class PatientInfo {
+  static int? patID;
+  static String? firstName;
+  static String? lastName;
+  static String? phone;
+  static String? sex;
+  static int? age;
+  static String? address;
+  static String? maritalStatus;
+  static String? bloodGroup;
+  static String? regDate;
+}
