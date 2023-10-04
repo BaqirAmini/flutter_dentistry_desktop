666c3aeda2d1c7c48c3e38ee9d359b8fbb7602d0
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-10-04 15:56:25 +0430

    Create this file to contain all translations in three languages.

---
+++ lib/config/translations.dart
@@ -0,0 +1,15 @@
+final Map<String, Map<String, String>> translations = {
+  'English': {
+    'chooseLanguage': 'Choose Language',
+    // Add other English translations here
+  },
+  'دری': {
+    'chooseLanguage': 'انتخاب زبان',
+    // Add other Dari translations here
+  },
+  'پښتو': {
+    'chooseLanguage': 'د ژبې انتخاب',
+    // Add other Dari translations here
+  },
+  // Add other languages here
+};
