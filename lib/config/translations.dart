e6c2dbcd26e03b9ff3d241e7e600f5b47751a96d
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-10-07 06:39:44 +0430

    Add translations for sidebar and dashboard.

--- lib/config/translations.dart
+++ lib/config/translations.dart
@@ -1,14 +1,68 @@
 final Map<String, Map<String, String>> translations = {
   'English': {
     'chooseLanguage': 'Choose Language',
+    /*----- DASHBOARD -----*/
+    'TodayPatient': 'Today\'s Patients',
+    'CurrentMonthExpenses': 'Current Month Expenses',
+    'CurrentYearTaxes': 'Current Year Taxes',
+    'AllPatients': 'All Patients',
+    'LastThreeMonthExpenses': 'Last Three Months Expenses',
+    'LastSixMonthPatients': 'Patients come in the last six months',
+    /*-----/. DASHBOARD -----*/
+    /*----- SIDEBAR -----*/
+    'Dashboard': 'Dashboard',
+    'Patients': 'Patients',
+    'Staff': 'Staff',
+    'Services': 'Services',
+    'Expenses': 'Expenses',
+    'Taxes': 'Taxes',
+    'Settings': 'Settings',
+    'Logout': 'Log out',
+    /*-----/.SIDEBAR  -----*/
     // Add other English translations here
   },
   'دری': {
     'chooseLanguage': 'انتخاب زبان',
+     /*----- DASHBOARD -----*/
+    'TodayPatient': 'مریض های امروز',
+    'CurrentMonthExpenses': 'مصارف ماه جاری',
+    'CurrentYearTaxes': 'مالیات امسال',
+    'AllPatients': 'همه مریض ها',
+    'LastThreeMonthExpenses': 'مصارف سه ماه اخیر',
+    'LastSixMonthPatients': 'مراجعه مریض ها در شش ماه اخیر',
+    /*-----/. DASHBOARD -----*/
+    /*----- SIDEBAR -----*/
+    'Dashboard': 'داشبورد',
+    'Patients': 'مریض ها',
+    'Staff': 'کارمندان',
+    'Services': 'خدمات',
+    'Expenses': 'مصارف',
+    'Taxes': 'مالیات',
+    'Settings': 'تنظیمات',
+    'Logout': 'خروج',
+    /*-----/. SIDEBAR -----*/
     // Add other Dari translations here
   },
   'پښتو': {
     'chooseLanguage': 'د ژبې انتخاب',
+     /*----- DASHBOARD -----*/
+    'TodayPatient': 'د نن ورځې ناروغان',
+    'CurrentMonthExpenses': 'د روانې میاشتې لګښتونه',
+    'CurrentYearTaxes': 'د سږ کال مالیه',
+    'AllPatients': 'ټول ناروغان',
+    'LastThreeMonthExpenses': 'د تیرو دریو میاشتو لګښتونه',
+    'LastSixMonthPatients': 'په تیرو شپږو میاشتو کې د ناروغانو لیدنه',
+    /*-----/. DASHBOARD -----*/
+    /*----- SIDEBAR -----*/
+    'Dashboard': 'ډشبورډ',
+    'Patients': 'ناروغان',
+    'Staff': 'کارمندان',
+    'Services': 'خدمتونه',
+    'Expenses': 'لګښتونه',
+    'Taxes': 'مالیات',
+    'Settings': 'ترتیبات',
+    'Logout': 'وتل',
+    /*-----/. SIDEBAR -----*/
     // Add other Dari translations here
   },
   // Add other languages here
