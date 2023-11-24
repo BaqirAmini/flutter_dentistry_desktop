e8eb018e1282df1b03857de03e9906acbb737691
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-11-24 20:47:44 +0430

    Create a variable for adults' selected teeth.

--- lib/views/patients/tooth_selection_info.dart
+++ lib/views/patients/tooth_selection_info.dart
@@ -2,4 +2,5 @@ class Tooth {
   static bool adultToothSelected = false;
   static bool childToothSelected = false;
   static String selectedChildTeeth = '';
+  static String selectedAdultTeeth = '';
 }
