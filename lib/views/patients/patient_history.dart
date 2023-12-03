a1dfc5fef75f1333fe6d6345c53958355397b786
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-12-03 21:50:11 +0430

    Fetch patient health history and display into listview.

--- lib/views/patients/patient_history.dart
+++ lib/views/patients/patient_history.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dentistry/models/db_conn.dart';
+import 'package:flutter_dentistry/views/patients/patient_info.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
@@ -17,7 +19,9 @@ class PatientHistory extends StatelessWidget {
         child: Scaffold(
           appBar: AppBar(
             title: const Text('Patient Health History'),
-            leading: IconButton(onPressed: () => Navigator.pop(context), icon: const BackButtonIcon()),
+            leading: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: const BackButtonIcon()),
             actions: [
               Tooltip(
                 message: 'افزودن جلسه جدید',
@@ -54,66 +58,84 @@ class PatientHistory extends StatelessWidget {
   }
 }
 
-class _HistoryContent extends StatelessWidget {
+class _HistoryContent extends StatefulWidget {
+  @override
+  State<_HistoryContent> createState() => _HistoryContentState();
+}
+
+class _HistoryContentState extends State<_HistoryContent> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            '3 نتیجه مثبت',
-            style: Theme.of(context).textTheme.labelMedium,
-          ),
-        ),
-        HoverCard(
-          title: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 34, 145, 38),
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          FontAwesomeIcons.heartPulse,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10.0),
-                    Text(
-                      'آیا دچار افت فشار خون و یا بلند رفتن فشار خون که موجب بعضی علایم یا مشکلات گردد، هستید؟',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                      textAlign: TextAlign.start,
-                    ),
-                  ],
+    return FutureBuilder(
+        future: _getHistory(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final histories = snapshot.data;
+            return ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '3 نتیجه مثبت',
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
                 ),
-                Row(
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: Icon(
-                          FontAwesomeIcons.plus,
-                          color: Colors.white,
-                          size: 14,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                /* SizedBox(
+                for (var h in histories!)
+                  HoverCard(
+                    title: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                decoration: const BoxDecoration(
+                                  color: Color.fromARGB(255, 34, 145, 38),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    FontAwesomeIcons.heartPulse,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10.0),
+                              Text(
+                                h.condName.toString(),
+                                style: Theme.of(context).textTheme.bodyLarge,
+                                textAlign: TextAlign.start,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color:
+                                      h.result == 1 ? Colors.red : Colors.blue,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: h.result == 1
+                                      ? const Icon(
+                                          FontAwesomeIcons.plus,
+                                          color: Colors.white,
+                                          size: 14,
+                                        )
+                                      : const Icon(
+                                          FontAwesomeIcons.minus,
+                                          color: Colors.white,
+                                          size: 14,
+                                        ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          /* SizedBox(
                   width: 100,
                   child: PopupMenuButton(
                     padding: EdgeInsets.zero,
@@ -143,91 +165,107 @@ class _HistoryContent extends StatelessWidget {
                   ),
                 ),
  */
+                        ],
+                      ),
+                    ),
+                    child: ListTile(
+                      title: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text('Diagnosis Date',
+                                    style:
+                                        Theme.of(context).textTheme.labelLarge),
+                                const SizedBox(width: 15.0),
+                                Expanded(
+                                  child: Text(
+                                    h.dianosisDate!,
+                                    textAlign: TextAlign.end,
+                                    style: const TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 112, 112, 112)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Text('Severty',
+                                    style:
+                                        Theme.of(context).textTheme.labelLarge),
+                                const SizedBox(width: 15.0),
+                                Expanded(
+                                  child: Text(
+                                    h.severty!,
+                                    textAlign: TextAlign.end,
+                                    style: const TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 112, 112, 112)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Text('Duration',
+                                    style:
+                                        Theme.of(context).textTheme.labelLarge),
+                                const SizedBox(width: 15.0),
+                                Expanded(
+                                  child: Text(
+                                    h.duration!,
+                                    textAlign: TextAlign.end,
+                                    style: const TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 112, 112, 112)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Text('Description',
+                                    style:
+                                        Theme.of(context).textTheme.labelLarge),
+                                const SizedBox(width: 15.0),
+                                Expanded(
+                                  child: Text(
+                                    h.notes!.isEmpty || h.notes == null
+                                        ? '--'
+                                        : h.notes.toString(),
+                                    textAlign: TextAlign.end,
+                                    style: const TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 112, 112, 112)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
               ],
-            ),
-          ),
-          child: ListTile(
-            title: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('Diagnosis Date',
-                          style: Theme.of(context).textTheme.labelLarge),
-                      const SizedBox(width: 15.0),
-                      const Expanded(
-                        child: Text(
-                          '2023-06-08',
-                          textAlign: TextAlign.end,
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 112, 112, 112)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text('Severty',
-                          style: Theme.of(context).textTheme.labelLarge),
-                      const SizedBox(width: 15.0),
-                      const Expanded(
-                        child: Text(
-                          'Medium',
-                          textAlign: TextAlign.end,
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 112, 112, 112)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text('Duration',
-                          style: Theme.of(context).textTheme.labelLarge),
-                      const SizedBox(width: 15.0),
-                      const Expanded(
-                        child: Text(
-                          '2 Months',
-                          textAlign: TextAlign.end,
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 112, 112, 112)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text('Description',
-                          style: Theme.of(context).textTheme.labelLarge),
-                      const SizedBox(width: 15.0),
-                      const Expanded(
-                        child: Text(
-                          'Some text here to describe it',
-                          textAlign: TextAlign.end,
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 112, 112, 112)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error occured: ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
   }
 }
 
@@ -235,7 +273,7 @@ class HoverCard extends StatefulWidget {
   final Widget title;
   final Widget child;
 
-  HoverCard({required this.title, required this.child});
+  const HoverCard({required this.title, required this.child});
 
   @override
   _HoverCardState createState() => _HoverCardState();
@@ -267,4 +305,51 @@ class _HoverCardState extends State<HoverCard> {
       ),
     );
   }
+}
+
+Future<List<HistoryDataModel>> _getHistory() async {
+  final conn = await onConnToDb();
+  final results = await conn.query('''
+SELECT d.cond_detail_ID, c.name, d.result, d.severty, d.duration, DATE_FORMAT(d.diagnosis_date, '%Y-%m-%d'), d.notes FROM conditions c 
+INNER JOIN condition_details d ON c.cond_ID = d.cond_ID WHERE d.pat_ID = ?''',
+      [PatientInfo.patID]);
+
+  final histories = results
+      .map(
+        (row) => HistoryDataModel(
+            condDetailID: row[0],
+            condName: row[1].toString(),
+            result: row[2],
+            severty: row[3] ?? "--",
+            duration: row[4] ?? "--",
+            dianosisDate: row[5].toString().isEmpty || row[5] == null
+                ? '--'
+                : row[5].toString(),
+            notes: row[6].toString().isEmpty || row[6] == null
+                ? '--'
+                : row[6].toString()),
+      )
+      .toList();
+  await conn.close();
+  return histories;
+}
+
+// Create a data model class for patient health history
+class HistoryDataModel {
+  final int condDetailID;
+  final String condName;
+  final int result;
+  final String? severty;
+  final String? duration;
+  final String? dianosisDate;
+  final String? notes;
+
+  HistoryDataModel(
+      {required this.condDetailID,
+      required this.condName,
+      required this.result,
+      this.severty,
+      this.duration,
+      this.dianosisDate,
+      this.notes});
 }
