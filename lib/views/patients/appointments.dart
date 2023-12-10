de264ed312e6df8dd1e874b1d1135804d8c1e0eb
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-12-10 15:40:50 +0430

    WIP, update to group appointment  by date.

--- lib/views/patients/appointments.dart
+++ lib/views/patients/appointments.dart
@@ -103,7 +103,7 @@ class _AppointmentState extends State<Appointment> {
   }
 }
 
-class _AppointmentContent extends StatelessWidget {
+/* class _AppointmentContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -266,6 +266,186 @@ class _AppointmentContent extends StatelessWidget {
     );
   }
 }
+ */
+
+class _AppointmentContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getAppointment(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // var appoints = snapshot.data;
+          Map<String, List<AppointmentDataModel>>? appoints = snapshot.data;
+          return ListView(
+            children: <Widget>[
+              for (var entry in appoints!.entries)
+                for (var a in entry.value)
+                  Card(
+                    child: Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          color: Colors
+                              .grey[200], // Change this to your desired color
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            a.meetDate,
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        ExpandableCard(
+                          title: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.green,
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                            color: Colors.green, width: 2.0),
+                                      ),
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Icon(
+                                          Icons.calendar_today_outlined,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10.0),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          a.serviceName,
+                                          style:
+                                              const TextStyle(fontSize: 18.0),
+                                        ),
+                                        Text(
+                                          a.meetDate,
+                                          style: const TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 12.0),
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Paid: ${a.paidAmount}',
+                                      style: const TextStyle(
+                                          color: Colors.grey, fontSize: 12.0),
+                                    ),
+                                    Text(
+                                      'Due: ${a.dueAmount}',
+                                      style: const TextStyle(
+                                          color: Colors.red, fontSize: 12.0),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          child: FutureBuilder(
+                            future: _getServices(a.serviceID),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                var services = snapshot.data;
+                                for (var service in services!) {
+                                  return ListTile(
+                                    title: Column(children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            const Text(
+                                              'Service Name',
+                                              style: TextStyle(
+                                                  fontSize: 12.0,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Text(
+                                              a.serviceName,
+                                              style: const TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 112, 112, 112),
+                                                fontSize: 12.0,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      for (var req
+                                          in service.requirements.entries)
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                  req.key == 'Teeth Selection'
+                                                      ? 'Teeth Selected'
+                                                      : req.key,
+                                                  style: const TextStyle(
+                                                      fontSize: 12.0,
+                                                      fontWeight:
+                                                          FontWeight.bold)),
+                                              const SizedBox(width: 15.0),
+                                              Expanded(
+                                                child: Text(
+                                                  req.key == 'Teeth Selection'
+                                                      ? req.value
+                                                          .split(',')
+                                                          .map(
+                                                              codeToDescription)
+                                                          .join(', ')
+                                                      : req.value,
+                                                  textAlign: TextAlign.end,
+                                                  style: const TextStyle(
+                                                    color: Color.fromARGB(
+                                                        255, 112, 112, 112),
+                                                    fontSize: 12.0,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                    ]),
+                                  );
+                                }
+                              } else if (snapshot.hasError) {
+                                return Text('Error: ${snapshot.error}');
+                              } else {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+                              return Text('ssss');
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
 
 class ExpandableCard extends StatelessWidget {
   final Widget title;
@@ -274,19 +454,17 @@ class ExpandableCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ExpansionTile(
-        title: title,
-        children: <Widget>[
-          child,
-        ],
-      ),
+    return ExpansionTile(
+      title: title,
+      children: <Widget>[
+        child,
+      ],
     );
   }
 }
 
 // Fetch appointment details
-Future<List<AppointmentDataModel>> getAppointment() async {
+/* Future<List<AppointmentDataModel>> getAppointment() async {
   final conn = await onConnToDb();
   final results = await conn.query(
     '''SELECT p.firstname, p.lastname, 
@@ -313,6 +491,48 @@ Future<List<AppointmentDataModel>> getAppointment() async {
           dueAmount: row[5],
           installment: row[7]))
       .toList();
+  await conn.close();
+  return appoints;
+}
+ */
+
+Future<Map<String, List<AppointmentDataModel>>> getAppointment() async {
+  final conn = await onConnToDb();
+  final results = await conn.query(
+    '''SELECT p.firstname, p.lastname, 
+        a.staff_ID, DATE_FORMAT(a.meet_date, "%Y-%m-%d"), a.paid_amount, a.due_amount, a.round, a.installment, 
+        a.discount, a.apt_ID, st.firstname, st.lastname, s.ser_name, s.ser_ID, a.round, a.installment, a.discount FROM patients p 
+        INNER JOIN appointments a ON a.pat_ID = p.pat_ID
+        INNER JOIN staff st ON a.staff_ID = st.staff_ID
+        INNER JOIN services s ON s.ser_ID = a.service_ID
+         WHERE a.pat_ID = ?''',
+    [PatientInfo.patID],
+  );
+
+  Map<String, List<AppointmentDataModel>> appoints = {};
+
+  for (var row in results) {
+    String meetDate = row[3].toString();
+    var appointment = AppointmentDataModel(
+      staffID: row[2],
+      staffFirstName: row[10],
+      staffLastName: row[11],
+      serviceName: row[12],
+      serviceID: row[13],
+      aptID: row[9],
+      meetDate: meetDate,
+      round: row[6],
+      paidAmount: row[4],
+      dueAmount: row[5],
+      installment: row[7],
+    );
+    if (appoints.containsKey(meetDate)) {
+      appoints[meetDate]!.add(appointment);
+    } else {
+      appoints[meetDate] = [appointment];
+    }
+  }
+
   await conn.close();
   return appoints;
 }
