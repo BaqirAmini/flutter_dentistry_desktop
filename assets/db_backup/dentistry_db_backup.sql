c4daf6b8a29ee262b1b1e536c00ab8be3e416b9c
Engineer Baqir Amini <baqir.amini01@gmail.com>
2023-12-10 09:16:26 +0430

    Modify to add create/use database at the top of it.

--- assets/db_backup/dentistry_db_backup.sql
+++ assets/db_backup/dentistry_db_backup.sql
@@ -7,6 +7,10 @@
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
+-- Create database
+CREATE DATABASE IF NOT EXISTS dentistry_db;
+USE dentistry_db;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
