11259f93e487222f0ef0d32ef888e1fd9fef19bf
Engineer Baqir Amini <baqir.amini01@gmail.com>
2024-01-03 22:14:27 +0430

    Update interface design of xrays screen.

--- lib/views/patients/xrays.dart
+++ lib/views/patients/xrays.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
         title: const Text('Image Collage'),
+        actions: [
+          IconButton(
+            tooltip: 'Add an X-Ray Image',
+            splashRadius: 28.0,
+            onPressed: () {},
+            icon: const Icon(Icons.add_photo_alternate_outlined, color: Colors.white),
+          ),
+          const SizedBox(width: 10.0),
+        ],
       ),
       body: XRayUploadScreen(),
     ),
@@ -21,6 +31,10 @@ class XRayUploadScreen extends StatelessWidget {
     'assets/xrays/image2.jpg',
     'assets/xrays/image3.jpg',
     'assets/xrays/image4.jpg',
+    'assets/xrays/image1.jpg',
+    'assets/xrays/image2.jpg',
+    'assets/xrays/image3.jpg',
+    'assets/xrays/image4.jpg',
     // Add more image paths
   ];
 
@@ -28,30 +42,38 @@ class XRayUploadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: images.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4, // Adjust number of images per row
-        crossAxisSpacing: 10.0, // Adjust horizontal spacing
-        mainAxisSpacing: 10.0, // Adjust vertical spacing
-      ),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    ImageViewer(images: images, initialIndex: index),
+    return Center(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.8,
+        child: GridView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4, // Adjust number of images per row
+            crossAxisSpacing: 10.0, // Adjust horizontal spacing
+            mainAxisSpacing: 10.0, // Adjust vertical spacing
+          ),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        ImageViewer(images: images, initialIndex: index),
+                  ),
+                );
+              },
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                child: Image.asset(images[index], fit: BoxFit.cover),
               ),
             );
           },
-          child: Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-            child: Image.asset(images[index], fit: BoxFit.cover),
-          ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
@@ -79,7 +101,7 @@ class _ImageViewerState extends State<ImageViewer> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Image Viewer'),
+        title: const Text('Image Viewer'),
       ),
       body: Stack(
         children: <Widget>[
@@ -88,10 +110,17 @@ class _ImageViewerState extends State<ImageViewer> {
             itemCount: widget.images.length,
             itemBuilder: (context, index) {
               return InteractiveViewer(
-                boundaryMargin: EdgeInsets.all(20.0),
+                boundaryMargin: const EdgeInsets.all(20.0),
                 minScale: 0.1,
                 maxScale: 2.0,
-                child: Image.asset(widget.images[index]),
+                child: Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: MediaQuery.of(context).size.height * 4.0,
+                    child:
+                        Image.asset(widget.images[index], fit: BoxFit.contain),
+                  ),
+                ),
               );
             },
           ),
@@ -99,28 +128,38 @@ class _ImageViewerState extends State<ImageViewer> {
             top: 0,
             bottom: 0,
             left: 0,
-            child: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                controller.previousPage(
-                  duration: Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                );
-              },
+            child: Container(
+              margin: const EdgeInsets.only(left: 30.0),
+              child: IconButton(
+                splashRadius: 30.0,
+                icon: const Icon(Icons.arrow_back_ios_new_rounded,
+                    color: Colors.grey),
+                onPressed: () {
+                  controller.previousPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
             ),
           ),
           Positioned(
             top: 0,
             bottom: 0,
             right: 0,
-            child: IconButton(
-              icon: Icon(Icons.arrow_forward),
-              onPressed: () {
-                controller.nextPage(
-                  duration: Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                );
-              },
+            child: Container(
+              margin: const EdgeInsets.only(right: 30.0),
+              child: IconButton(
+                splashRadius: 30.0,
+                icon: const Icon(Icons.arrow_forward_ios_rounded,
+                    color: Colors.grey),
+                onPressed: () {
+                  controller.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
             ),
           ),
         ],
