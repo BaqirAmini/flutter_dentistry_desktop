2965e357655479e18706139c17c7290fdc72473e
Engineer Baqir Amini <baqir.amini01@gmail.com>
2024-01-27 07:05:03 +0430

    Modified to create a static function for staff photo upload.

--- lib/views/staff/staff_info.dart
+++ lib/views/staff/staff_info.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:flutter_dentistry/models/db_conn.dart';
 import 'package:galileo_mysql/src/blob.dart';
 
 class StaffInfo {
@@ -17,7 +18,7 @@ class StaffInfo {
   static Uint8List? contractFile;
   static String? fileType;
 
-   // position types dropdown variables
+  // position types dropdown variables
   static String staffDefaultPosistion = 'داکتر دندان';
   static var staffPositionItems = [
     'داکتر دندان',
@@ -26,4 +27,20 @@ class StaffInfo {
     'حسابدار',
     'کار آموز'
   ];
+
+  static Uint8List? uint8list;
+  // This function fetches staff photo
+  static Future<void> onFetchStaffPhoto(int staffID) async {
+    final conn = await onConnToDb();
+    final result = await conn
+        .query('SELECT photo FROM staff WHERE staff_ID = ?', [staffID]);
+
+    Blob? staffPhoto =
+        result.first['photo'] != null ? result.first['photo'] as Blob : null;
+
+    // Convert image of BLOB type to binary first.
+    uint8list =
+        staffPhoto != null ? Uint8List.fromList(staffPhoto.toBytes()) : null;
+    await conn.close();
+  }
 }
