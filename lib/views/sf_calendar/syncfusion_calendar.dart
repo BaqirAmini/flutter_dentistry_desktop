6b169fc9a07d1ba2d37da6376f1928a035d8ceaa
Engineer Baqir Amini <baqir.amini01@gmail.com>
2024-01-29 15:42:23 +0430

    Add an alert dialog with some fields when choosing a date.

--- lib/views/sf_calendar/syncfusion_calendar.dart
+++ lib/views/sf_calendar/syncfusion_calendar.dart
@@ -33,6 +33,7 @@ class CalendarPage extends StatefulWidget {
 
 class _CalendarPageState extends State<CalendarPage> {
   late List<Appointment> _appointments;
+  final _calFormKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -55,8 +56,234 @@ class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     return SfCalendar(
-      view: CalendarView.week,
-      dataSource: _AppointmentDataSource(_appointments),
+      view: CalendarView.day,
+      onTap: (CalendarTapDetails details) {
+        if (details.targetElement == CalendarElement.calendarCell) {
+          _showDateTimeDialog(context);
+        }
+      },
+    );
+  }
+
+  _showDateTimeDialog(BuildContext context) async {
+    DateTime selectedDateTime = DateTime.now();
+    TextEditingController _apptdatetimeController = TextEditingController();
+    TextEditingController _commentController = TextEditingController();
+    TextEditingController _titleController = TextEditingController();
+    String dropdownValue = '15 Minutes';
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Set Details'),
+          content: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: Form(
+              key: _calFormKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 10.0),
+                    child: TextFormField(
+                      controller: _titleController,
+                      validator: (value) {
+                        if (value!.isNotEmpty) {
+                          if (value.length < 5 || value.length > 40) {
+                            return 'Details should at least 5 and at most 40 characters.';
+                          }
+                          return null;
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'عنوان جلسه',
+                        suffixIcon: Icon(Icons.title_outlined),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.grey)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                        errorBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.red)),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          borderSide: BorderSide(color: Colors.red, width: 1.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 10.0),
+                    child: TextFormField(
+                      controller: _apptdatetimeController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Date and time should be set.';
+                        }
+                        return null;
+                      },
+                      readOnly: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'تاریخ و زمان جلسه',
+                        suffixIcon: Icon(Icons.access_time),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.grey)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                        errorBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.red)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide:
+                                BorderSide(color: Colors.red, width: 1.5)),
+                      ),
+                      onTap: () async {
+                        final DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        );
+                        if (pickedDate != null) {
+                          // ignore: use_build_context_synchronously
+                          final TimeOfDay? pickedTime = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.now(),
+                          );
+                          if (pickedTime != null) {
+                            selectedDateTime = DateTime(
+                              pickedDate.year,
+                              pickedDate.month,
+                              pickedDate.day,
+                              pickedTime.hour,
+                              pickedTime.minute,
+                            );
+                            _apptdatetimeController.text =
+                                selectedDateTime.toString();
+                          }
+                        }
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 10.0),
+                    child: TextFormField(
+                      controller: _commentController,
+                      validator: (value) {
+                        if (value!.isNotEmpty) {
+                          if (value.length < 5 || value.length > 40) {
+                            return 'Details should at least 5 and at most 40 characters.';
+                          }
+                          return null;
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'توضیحات',
+                        suffixIcon: Icon(Icons.description_outlined),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.grey)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.blue)),
+                        errorBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            borderSide: BorderSide(color: Colors.red)),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          borderSide: BorderSide(color: Colors.red, width: 1.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 10.0),
+                    child: InputDecorator(
+                      decoration: const InputDecoration(
+                        suffixIcon: Icon(Icons.notifications_active_outlined),
+                        border: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: SizedBox(
+                          height: 26.0,
+                          child: DropdownButton(
+                            // isExpanded: true,
+                            icon: const Icon(Icons.arrow_drop_down),
+                            value: dropdownValue,
+                            items: <String>[
+                              '5 Minutes',
+                              '15 Minutes',
+                              '30 Minutes',
+                              '1 Hour',
+                              '2 Hours'
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Save'),
+              onPressed: () {
+                if (_calFormKey.currentState!.validate()) {}
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
