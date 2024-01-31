e343a83a020e610071e09ab3eb1264f04eba0925
Engineer Baqir Amini <baqir.amini01@gmail.com>
2024-01-31 11:47:34 +0430

    Move fetchStaff function here to be used globally.

--- lib/config/global_usage.dart
+++ lib/config/global_usage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dentistry/models/db_conn.dart';
 
 class GlobalUsage {
   // A toast message to be used anywhere required
@@ -15,14 +16,35 @@ class GlobalUsage {
 
   /* ------------------- CHARACTERS/DIGITS ALLOWED ---------------- */
   // 0-9 and + are allowed
-   static const allowedDigits = "[0-9+۰-۹]";
+  static const allowedDigits = "[0-9+۰-۹]";
   //  alphabetical letters both in English & Persian allowed including comma
   static const allowedEPChar = "[a-zA-Z,، \u0600-\u06FFF]";
   // 0-9 and period(.) are allowed
   static const allowedDigPeriod = "[0-9.]";
   /* -------------------/. CHARACTERS/DIGITS ALLOWED ---------------- */
- 
-//  This static variable specifies whether the appointment 
+
+//  This static variable specifies whether the appointment
 //is created with creating a new patient or an existing patient (true = new patient created, false = patient already existing)
- static bool newPatientCreated = true;
+  static bool newPatientCreated = true;
+
+  // Fetch staff which will be needed later.
+  Future<List<Map<String, dynamic>>> fetchStaff() async {
+    // Fetch staff for purchased by fields
+    var conn = await onConnToDb();
+    var results = await conn.query(
+        'SELECT staff_ID, firstname, lastname FROM staff WHERE position = ?',
+        ['داکتر دندان']);
+
+    List<Map<String, dynamic>> staffList = results
+        .map((result) => {
+              'staff_ID': result[0].toString(),
+              'firstname': result[1],
+              'lastname': result[2] ?? ''
+            })
+        .toList();
+
+    await conn.close();
+
+    return staffList;
+  }
 }
