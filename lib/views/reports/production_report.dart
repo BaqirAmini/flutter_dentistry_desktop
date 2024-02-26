56f224eb8f7244e68ce49c4f20a0589820737028
Engineer Baqir Amini <baqir.amini01@gmail.com>
2024-02-26 13:59:27 +0430

    Setup pdf & excel related packages for report outputs.

--- lib/views/reports/production_report.dart
+++ lib/views/reports/production_report.dart
@@ -1,62 +1,119 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dentistry/models/db_conn.dart';
+import 'package:flutter_dentistry/views/settings/settings_menu.dart';
+import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:excel/excel.dart';
 import 'package:path/path.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart' hide Column;
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 void main() => runApp(const ProductionReport());
 
 class ProductionReport extends StatelessWidget {
   const ProductionReport({super.key});
 
+// This is function generates excel output
   void createExcel() async {
-    // Create a new Excel package
-    var excel = Excel.createExcel();
+    final conn = await onConnToDb();
 
-    // Access the 'Sheet1'
-    var sheet = excel['Sheet1'];
+    // Query data from the database.
+    var results = await conn.query('SELECT firstname, lastname FROM patients');
 
-    // Populate the sheet with sample data
-    var data = <List<String>>[
-      [
-        'Patient ID',
-        'Patient Name',
-        'Procedure Code',
-        'Procedure Description',
-        'Procedure Date',
-        'Dentist',
-        'Cost'
-      ],
-      [
-        '001',
-        'John Doe',
-        'PC001',
-        'Tooth Extraction',
-        '2024-02-18',
-        'Dr. Smith',
-        '\$200'
-      ],
-      // Add more data rows here
-    ];
+    // Create a new Excel document.
+    final Workbook workbook = Workbook();
+    final Worksheet sheet = workbook.worksheets[0];
 
-    for (var i = 0; i < data.length; i++) {
-      var row = data[i];
-      for (var j = 0; j < row.length; j++) {
-        sheet
-            .cell(CellIndex.indexByColumnRow(columnIndex: j, rowIndex: i))
-            .value = row[j].toString() as CellValue?;
+    // Populate the sheet with data from the database.
+    var rowIndex = 0;
+    for (var row in results) {
+      for (var i = 0; i < row.length; i++) {
+        sheet.getRangeByIndex(rowIndex + 1, i + 1).setText(row[i].toString());
       }
+      rowIndex++;
     }
 
-    // Save the Excel file
-    var output = await getTemporaryDirectory();
-    var file = "${output.path}/example.xlsx";
+    // Save the Excel file.
+    final List<int> bytes = workbook.saveAsStream();
 
-    var bytes = excel.encode();
-    File(file)
-      ..createSync(recursive: true)
-      ..writeAsBytesSync(bytes!);
+    // Get the directory to save the Excel file.
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String path = directory.path;
+    final File file = File('$path/Output.xlsx');
+
+    // Write the Excel file.
+    await file.writeAsBytes(bytes, flush: true);
+
+    // Open the file
+    await OpenFile.open(file.path);
+
+    // Close the database connection.
+    await conn.close();
+  }
+
+  void createPdf() async {
+    try {
+      // Create a new PDF document.
+      final PdfDocument document = PdfDocument();
+
+      // Create a new page in the document.
+      final PdfPage page = document.pages.add();
+
+      // Create a new PDF grid.
+      final PdfGrid grid = PdfGrid();
+
+      // Set the text direction for Persian text.
+      final PdfStringFormat format = PdfStringFormat();
+      format.textDirection = PdfTextDirection.rightToLeft;
+      format.alignment = PdfTextAlignment.right;
+
+      final conn = await onConnToDb();
+
+      // Query data from the database.
+      var results =
+          await conn.query('SELECT firstname, lastname FROM patients');
+      final ByteData fontData =
+          (await rootBundle.load('assets/fonts/per_sans_font.ttf'));
+      final PdfTrueTypeFont ttfFont =
+          PdfTrueTypeFont(fontData.buffer.asUint8List(), 12);
+      // Populate the grid with data from the database.
+      grid.columns.add(count: results.length);
+      for (final row in results) {
+        final PdfGridRow pdfRow = grid.rows.add();
+        for (var i = 0; i < row.length; i++) {
+          pdfRow.cells[i].value = row[i].toString();
+          pdfRow.cells[i].style = PdfGridCellStyle(font: ttfFont);
+        }
+      }
+
+      // Draw the grid on the page.
+      grid.draw(
+        page: page,
+        bounds: Rect.fromLTWH(
+            0, 0, page.getClientSize().width, page.getClientSize().height),
+      );
+
+      // Save the PDF file.
+      final List<int> bytes = await document.save();
+
+      // Get the directory to save the PDF file.
+      final Directory directory = await getApplicationDocumentsDirectory();
+      final String path = directory.path;
+      final File file = File('$path/Output.pdf');
+
+      // Write the PDF file.
+      await file.writeAsBytes(bytes, flush: true);
+
+      // Open the file
+      await OpenFile.open(file.path);
+
+      // Close the database connection.
+      await conn.close();
+    } catch (e) {
+      print('Error with PDF: $e');
+    }
   }
 
   // This widget is the root of your application.
@@ -72,20 +129,7 @@ class ProductionReport extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               ElevatedButton(
-                onPressed: () async {
-                  final pdf = pw.Document();
-                  pdf.addPage(
-                    pw.Page(
-                      build: (pw.Context context) => pw.Center(
-                        child: pw.Text("Hello World",
-                            style: const pw.TextStyle(fontSize: 40)),
-                      ),
-                    ),
-                  );
-                  final output = await getTemporaryDirectory();
-                  final file = File("${output.path}/example.pdf");
-                  await file.writeAsBytes(await pdf.save());
-                },
+                onPressed: createPdf,
                 child: const Text('Generate PDF'),
               ),
               ElevatedButton(
